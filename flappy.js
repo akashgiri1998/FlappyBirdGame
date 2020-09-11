2a8c760ad92339f39c69978fcca0009a1cb34567
akashgiri1998 <akashgirigiri2@gmail.com>
2020-09-11 18:58:24 +0545

    'FlappyBirdGame'

---
+++ flappy.js
@@ -0,0 +1,28 @@
+const cvs = document.getElementById("canvas");
+const ctx = cvs.getContext("2d");
+
+//background
+const bg = new Image();
+bg.src = './images/bg.png';
+
+//foreground
+const fg = new Image();
+fg.src = './images/fg.png';
+
+//pipenorth
+const pipeNorth = new Image();
+pipeNorth.src = './images/pipeNorth.png';
+
+//pipeSouth
+const pipeSouth = new Image();
+pipeSouth.src = './images/pipeSouth.png';
+
+//bird
+const bird = new Image();
+bird.src = './images/bird.png';
+
+function draw(){
+    ctx.drawImage(bg,0,0);
+    ctx.drawImage(pipeNorth,100,0);
+}
+draw();
